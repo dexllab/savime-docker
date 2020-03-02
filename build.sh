f1f40f1397ed055fd72d21a532e179c2d3f8caf8
Daniel <danielnascramos@gmail.com>
2020-03-02 10:26:16 -0300

    Set usage of envs.

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-docker build -t savime --network=host --build-arg USER_ID=$(id -u)  --build-arg GROUP_ID=$(id -g) --build-arg NUM_THREADS_MAKE=4 --build-arg USERNAME="$USER" .
+export NUM_THREADS_MAKE=4
+docker build -t savime --network=host --build-arg USER_ID=$(id -u)  --build-arg GROUP_ID=$(id -g) --build-arg NUM_THREADS_MAKE="$NUM_THREADS_MAKE" --build-arg USERNAME="$USER" .
--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ start () {
         echo "Could not create the dir $HOST_SAVIME_DIR: the directory already exists. I'm proceeding. Please check if you own and have read/write/run permission over this directory." 
     fi
     mv /tmp/savime-socket  /tmp/savime-socket-bkp  > /dev/null 2>&1
-    docker run --rm -p 65000:65000 -v "$HOST_SAVIME_DIR":/dev/shm/savime -v /tmp:/tmp -u $(id -u ${USER}):$(id -g ${USER}) --name savime_container -d savime > /dev/null 2>&1 
+    docker run --rm -p "$HOST_SAVIME_PORT":65000 -v "$HOST_SAVIME_DIR":/dev/shm/savime -v /tmp:/tmp -u $(id -u ${USER}):$(id -g ${USER}) --name savime_container -d savime > /dev/null 2>&1 
     if [ $? -eq  0 ]; then
         echo "Savime container started succesfully."
     fi
