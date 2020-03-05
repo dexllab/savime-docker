54c7e2f4ad5873eafe6b6bcf83043f3b70f2817f
Daniel N. R. da Silva <danielnascramos@gmail.com>
2020-03-05 12:39:47 -0300

    Add command option to force build.

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 export NUM_THREADS_MAKE=4
-docker build -t savime --network=host --build-arg USER_ID=$(id -u)  --build-arg GROUP_ID=$(id -g) --build-arg NUM_THREADS_MAKE="$NUM_THREADS_MAKE" --build-arg USERNAME="$USER" .
+docker build --no-cache -t savime --network=host --build-arg USER_ID=$(id -u)  --build-arg GROUP_ID=$(id -g) --build-arg NUM_THREADS_MAKE="$NUM_THREADS_MAKE" --build-arg USERNAME="$USER" .
